fe0afd9a56dbbbb034cbc89836b8bbc3264383ab
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-13 17:02:56 -0500

    MVP. Form now clears upon submission.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import TodoList from "./components/TodoComponents/TodoList";
 import TodoForm from "./components/TodoComponents/TodoForm";
 
-const taskList = [
-  {
-    task: "Organize Garage",
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: "Bake Cookies",
-    id: 1528817084358,
-    completed: false
-  }
-];
+// const taskList = [
+//   {
+//     task: "Organize Garage",
+//     id: 1528817077286,
+//     completed: false
+//   },
+//   {
+//     task: "Bake Cookies",
+//     id: 1528817084358,
+//     completed: false
+//   }
+// ];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -22,7 +22,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      taskList: taskList,
+      taskList: [],
       task: ""
     };
   }
@@ -57,7 +57,7 @@ class App extends React.Component {
       <div className="container">
         <TodoList taskList={this.state.taskList} />
         <TodoForm
-          taskList={this.state.taskList}
+          task={this.state.task}
           handleChanges={this.handleChanges}
           addTask={this.addTask}
         />
